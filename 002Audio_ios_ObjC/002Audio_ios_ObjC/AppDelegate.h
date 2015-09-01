f71bd9d71020bfd2c3a31b68dd10dd0af993b009
Henry AT <yiperu@gmail.com>
2015-09-01 00:05:11 -0400

    Creation of second project for Audio in Objetive-C

---
+++ 002Audio_ios_ObjC/002Audio_ios_ObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  002Audio_ios_ObjC
+//
+//  Created by Henry AT on 9/1/15.
+//  Copyright (c) 2015 Apps4s. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 002Audio_ios_ObjC/002Audio_ios_ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  002Audio_ios_ObjC
+//
+//  Created by Henry AT on 9/1/15.
+//  Copyright (c) 2015 Apps4s. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
